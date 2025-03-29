8fe5de6065d6667a2a0ec4060a68f1330013af42
Vilorshini <ramsamyvilorshini@gmail.com>
2025-03-29 18:29:44 +0400

    Bug deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 import Main from "./components/main/Main"
-import Sidebar from "./components/sidebar/Sidebar"
+import Sidebar from "./components/sidebar/Sidebar.jsx"
 import "./scss/App.css"
 import "./config/gemini"
 import useThemeStore from "./stores/themeStore"
